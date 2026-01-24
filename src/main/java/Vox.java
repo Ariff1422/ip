2e9f809ac1bce88007bf945e1198b2c6f60e0875
Ariff Muhammed <ariffahsan@gmail.com>
2026-01-25 00:45:24 +0800

    Week 0: Added printLine function for ease of coding in the future

--- src/main/java/Vox.java
+++ src/main/java/Vox.java
@@ -1,18 +1,20 @@
 public class Vox {
+    public static void printLine() {
+        // For ease of calling the function and utilising the lines as section breaks
+        System.out.println("____________________________________________________________\n");
+    }
     public static void main(String[] args) {
         String name = "Vox";
-        // For separation between sections, use horizontalLine
-        String horizontalLine = "─────────────────────────────────────────────────────────────";
         String logo = "__      __          \n"
                 + "\\ \\    / /_  __  __ \n"
                 + " \\ \\  / / _ \\ \\ \\/ /\n"
                 + "  \\ \\/ / (_) | >  < \n"
                 + "   \\__/ \\___/ /_/\\_\\\n";
-        System.out.println(horizontalLine);
+        printLine();
         System.out.println("Hello! I'm\n" + logo);
         System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
+        printLine();
         System.out.println("Bye. Hope to see you again!");
-        System.out.println(horizontalLine);
+        printLine();
     }
 }
