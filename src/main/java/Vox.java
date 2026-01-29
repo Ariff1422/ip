e59e8785a524dc84b35c88ae9da9139af7b1ea6b
Ariff Muhammed <ariffahsan@gmail.com>
2026-01-29 18:06:49 +0800

    Added the list function where it keeps track of the tasks at hand and return the list once prompted

--- src/main/java/Vox.java
+++ src/main/java/Vox.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Vox {
     // Define Color Constants (ANSI Escape Codes)
     public static final String RESET = "\u001B[0m";
     public static final String GREEN = "\u001B[32m";
+
     public static void printLine() {
         // For ease of calling the function and utilising the lines as section breaks
         System.out.println("________________________________________________\n");
     }
+
     public static void printBreaks() {
         // For the separations between the echoing
         // RESET to prevent any unforeseen break in green colour
@@ -16,7 +19,10 @@ public class Vox {
 
     public static void main(String[] args) {
         String name = "Vox";
+        // Initialise Scanner
         Scanner in = new Scanner(System.in);
+        // Initialise a String array to maintain user list
+        ArrayList<String> userInputs = new ArrayList<String>();
         String logo = "__      __          \n"
                 + "\\ \\    / /_  __  __ \n"
                 + " \\ \\  / / _ \\ \\ \\/ /\n"
@@ -31,13 +37,22 @@ public class Vox {
             System.out.print(GREEN);
             line = in.nextLine();
             System.out.print(RESET);
+            if (line.trim().equalsIgnoreCase("list")) {
+                printBreaks();
+                for (int i = 0; i < userInputs.size(); i++) {
+                    System.out.println(("    " + (i + 1) + ". " + userInputs.get(i)));
+                }
+                printBreaks();
+            } else {
+                userInputs.add(line);
+                printBreaks();
+                System.out.println("    added: " + line);
+                printBreaks();
+            }
             if (line.trim().equalsIgnoreCase("bye")) {
                 printLine();
                 break;
             }
-            printBreaks();
-            System.out.println("    " + line);
-            printBreaks();
         }
         System.out.println("Bye. Hope to see you again!");
         printLine();
