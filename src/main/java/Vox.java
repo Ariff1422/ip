f0c1787e7a9be46e928d668c7abdbbb72cbb496d
Ariff Muhammed <ariffahsan@gmail.com>
2026-01-29 13:40:41 +0800

    Level 1: Added echoing and colour modification for the user input

--- src/main/java/Vox.java
+++ src/main/java/Vox.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class Vox {
+    // Define Color Constants (ANSI Escape Codes)
+    public static final String RESET = "\u001B[0m";
+    public static final String GREEN = "\u001B[32m";
     public static void printLine() {
         // For ease of calling the function and utilising the lines as section breaks
-        System.out.println("____________________________________________________________\n");
+        System.out.println("________________________________________________\n");
     }
+    public static void printBreaks() {
+        // For the separations between the echoing
+        // RESET to prevent any unforeseen break in green colour
+        System.out.println(RESET + "    ____________________________________________\n");
+    }
+
     public static void main(String[] args) {
         String name = "Vox";
+        Scanner in = new Scanner(System.in);
         String logo = "__      __          \n"
                 + "\\ \\    / /_  __  __ \n"
                 + " \\ \\  / / _ \\ \\ \\/ /\n"
@@ -14,6 +26,19 @@ public class Vox {
         System.out.println("Hello! I'm\n" + logo);
         System.out.println("What can I do for you?");
         printLine();
+        while (true) {
+            String line;
+            System.out.print(GREEN);
+            line = in.nextLine();
+            System.out.print(RESET);
+            if (line.trim().equalsIgnoreCase("bye")) {
+                printLine();
+                break;
+            }
+            printBreaks();
+            System.out.println("    " + line);
+            printBreaks();
+        }
         System.out.println("Bye. Hope to see you again!");
         printLine();
     }
